85a0a6dc46612bd2a3e152763d59ac6beae43dd5
BK Bolisetty <bk@blitzkraft.me>
2020-08-30 14:58:57 -0400

    Use increment to print consecutive characters
    Still very crude. no loops.

--- boot.asm
+++ boot.asm
@@ -1,33 +1,82 @@
 mov ah, 0x0e ; switch to teletype mode
-mov al, 'B' ; print this char in teletype
+mov al, 65 ; ASCII decimal value of 'A'
 int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 'K' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 's' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, ' ' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 'b' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 'o' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 'o' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 't' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 'e' ; print this char in teletype
-int 0x10 ; interrupt 0x10
-mov ah, 0x0e ; switch to teletype mode
-mov al, 'r' ; print this char in teletype
-int 0x10 ; interrupt 0x10
+
+inc al ; increment al register
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
+inc al 
+int 0x10
+
 jmp $
 
 times 510-($-$$) db 0
