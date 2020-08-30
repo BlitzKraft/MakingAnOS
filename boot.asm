854b503bdd127fd8d872a9e79db5f8724cf4e714
BK Bolisetty <bk@blitzkraft.me>
2020-08-30 14:53:42 -0400

    Print chars crudely
    very crudely

--- boot.asm
+++ boot.asm
@@ -1,3 +1,33 @@
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'B' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'K' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 's' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, ' ' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'b' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'o' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'o' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 't' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'e' ; print this char in teletype
+int 0x10 ; interrupt 0x10
+mov ah, 0x0e ; switch to teletype mode
+mov al, 'r' ; print this char in teletype
+int 0x10 ; interrupt 0x10
 jmp $
 
 times 510-($-$$) db 0
